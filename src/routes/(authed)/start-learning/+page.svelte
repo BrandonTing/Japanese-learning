620252eaf1474cff11c7fae366f83cc29dc12fbb
Brandon Ting <brandonting@kkcompany.com>
2025-01-23 15:54:39 +0800

    add signout

--- src/routes/(authed)/start-learning/+page.svelte
+++ src/routes/(authed)/start-learning/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { authClient } from '$lib/auth-client';
+	import { Button } from '@/components/ui/button';
 	const session = authClient.useSession();
 	const user = $derived($session.data?.user);
 </script>
 
 <div>{user?.id}</div>
+<Button
+	onclick={async () => {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					goto('/');
+				}
+			}
+		});
+	}}>Sign out</Button
+>
