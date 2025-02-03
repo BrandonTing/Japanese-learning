0b0cfc19e09b70a21a367ffdd78d4557e0a3ea88
Brandon Ting <brandonting@kkcompany.com>
2025-02-03 15:53:01 +0800

    integrate sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,15 @@
-import { auth } from "$lib/auth"; // path to your auth file
-import { svelteKitHandler } from "better-auth/svelte-kit";
- 
-export async function handle({ event, resolve }) {
-    return svelteKitHandler({ event, resolve, auth });
-}
+import { sequence } from '@sveltejs/kit/hooks';
+import * as Sentry from '@sentry/sveltekit';
+import { auth } from '$lib/auth'; // path to your auth file
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+Sentry.init({
+	dsn: 'https://7f4cb7b24d53cb22d87daa989cbed5f4@o4508754109530113.ingest.de.sentry.io/4508754157240400',
+	tracesSampleRate: 1
+});
+
+export const handleError = Sentry.handleErrorWithSentry();
+
+export const handle = sequence(Sentry.sentryHandle(), async function _handle({ event, resolve }) {
+	return svelteKitHandler({ event, resolve, auth });
+});
