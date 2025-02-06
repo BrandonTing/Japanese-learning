8635c1285f06f552dd336343e5d50db852881224
Brandon Ting <brandonting@kkcompany.com>
2025-02-06 14:39:21 +0800

    apply to portal components

--- src/lib/states/userSetting.svelte.ts
+++ src/lib/states/userSetting.svelte.ts
@@ -1,20 +1,29 @@
-import { type CursorOptions } from "@/cursorUtil";
+import { typeVariableNameMap, type CursorOptions } from "@/cursorUtil";
 
 type CursorType = keyof typeof CursorOptions
 
 const localStorageKey = "cursorType"
 
 class UserSetting {
-  cursorType = $state<CursorType>("default")
+  cursorType = $state<CursorType>("default");
   constructor() {
     const existing = localStorage.getItem(localStorageKey)
     if (existing) {
       this.cursorType = existing as CursorType
+      this.setCssVariable(this.cursorType)
     }
   }
   changeCursorType(type: CursorType) {
     this.cursorType = type
     localStorage.setItem(localStorageKey, type)
+  }
+  setCssVariable(cursorType: CursorType) {
+    Object.entries(typeVariableNameMap).forEach(value => {
+      const [key, { prefix, amount }] = value
+      for (let i = 1; i <= amount; i++) {
+        document.documentElement.style.setProperty(`--cursor${prefix ? `-${prefix}` : ""}-image-${i}`, `url("/cursor-image/${cursorType}/${key}/${i}.png")`)
+      }
+    })
   }
 }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import { Toaster } from '$lib/components/ui/sonner';
-	import { getImgsVariable } from '@/cursorUtil';
-	import { userSetting } from '@/states/userSetting.svelte';
 	import '../app.css';
 	let { children } = $props();
-	const cursorStyle = $derived(getImgsVariable(userSetting.cursorType));
 </script>
 
-<Toaster richColors position="bottom-center" />
+<div class="cursor w-lvw h-lvh">
+	<Toaster richColors position="bottom-center" />
 
-<div class="container mx-auto p-4" style={cursorStyle}>
-	{@render children()}
+	<div class="container mx-auto p-4">
+		{@render children()}
+	</div>
 </div>
